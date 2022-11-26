9d44d260b818078a6ccf1052298e2c5da75909a8
Codehash001 <codehashblockchain@gmail.com>
2022-11-26 19:33:42 +0530

    first commit

---
+++ pages/Aboutus.js
@@ -0,0 +1,21 @@
+import react from "react";
+import Navbar from "../components/Navbar";
+import About from "../components/About";
+
+
+function Aboutus () {
+    return (
+
+        <div>
+            <Navbar/>
+            <div>
+            <About/>
+            </div>
+            
+            
+
+        </div>
+    )
+}
+
+export default Aboutus
