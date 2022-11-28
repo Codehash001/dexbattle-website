224d117c80679e2befac7e4292191314d27be939
Codehash001 <codehashblockchain@gmail.com>
2022-11-28 23:43:47 +0530

    update privacy

--- pages/privacyPolicy.js
+++ pages/privacyPolicy.js
@@ -5,22 +5,29 @@ export default function Privacy() {
             <h1 className="text-blue-400 font-Kanit text-3xl">
                 DB Gaming Privacy Policy
             </h1>
+            <div>
             <p className="text-white text-2xl">
                 <span className="font-semibold">LAST UPDATED: November 2022</span>
+                <br/>
                 <br/>
                 DB Gaming Limited and its affiliates (“DB,” “we,” or “us”) want you to be familiar with how we collect,
                 use and disclose information. This Privacy Policy describes our practices in connection with information
                 that we collect through:
                 <br/>
+                <br/>
                 Websites operated by us from which you are accessing this Privacy Policy (the “Websites”);
+                <br/>
                 <br/>
                 HTML-formatted email messages that we send to you that link to this Privacy Policy or other
                 communications with you; and Offline business interactions you have with us.
                 <br/>
+                <br/>
                 Collectively, we refer to the Websites, emails, and offline business interactions as the “Services.”
                 <br/>
-                
+                <br/>
+
             </p>
+            </div>
         </div>
     )
 }
