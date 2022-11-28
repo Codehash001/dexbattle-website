267f3b305a43c641f50590eb7dc72af1aa1d3921
Codehash001 <codehashblockchain@gmail.com>
2022-11-28 23:35:52 +0530

    privacy policy

--- pages/privacyPolicy.js
+++ pages/privacyPolicy.js
@@ -2,99 +2,25 @@ export default function Privacy() {
 
     return(
         <div className="w-screen mx-16 flex flex-col items-center overflow-hidden">
-        <h1 className="text-white font-Kanit text-[30px] my-6">
-            DB Gaming privacy Policy
-        </h1>
-
-        <p className="my-4 text-gray-200 font-Kanit text-[24px] text-justify">
-            <span className="font-bold my-4 text-gray-200 font-Kanit text-[24px]">LAST UPDATED: November 2022</span><br/>
-DB Gaming Limited and its affiliates (“DB,” “we,” or “us”) want you to be familiar with how we collect, use and disclose information.
-This Privacy Policy describes our practices in connection with information that we collect through:
-<br/>
-Websites operated by us from which you are accessing this Privacy Policy (the “Websites”);
-<br/>
-HTML-formatted email messages that we send to you that link to this Privacy Policy or other communications with you; and Offline business interactions you have with us.
-Collectively, we refer to the Websites, emails, and offline business interactions as the “Services.”
-<br/>
-<br/>
-<span className="font-bold">PERSONAL INFORMATION</span>
-<br/>
-“Personal Information” is information that identifies you as an individual or relates to an identifiable individual. The Services collect Personal Information, including:
-Name, Email address, Username and password, Crypto wallet address, including related data used to detect cryptocurrency and NFT holdings, Social media handle, IP address
- (we may also derive your approximate location from your IP address)
- <br/>
-In-game activity, such as lands visited or acquired, quests and games completed, NFT transactions, and avatars created
-<br/>
-<span className="font-bold">Collection of Personal Information</span>
-<br/>
-
-<ul>
-<li>We and our service providers collect Personal Information in a variety of ways, including:</li>
-<li>Through the Services.</li>
-<li>We collect Personal Information through the Services, for example, when you register an account to access the Services, complete a survey, contact customer support,
-or make a purchase on our online marketplace.</li>
-<li>From Other Sources.</li>
-<li>We receive your Personal Information from other sources, for example, crypto wallet providers, when they share the information with us.</li>
-</ul>
-
-Use of Personal Information
-<br/>
-We and our service providers use Personal Information for the following purposes:
-<br/>
-Providing the functionality of the Services and fulfilling your requests.
-<ul>
-    
-<li>To provide the Services&apos; functionality to you, such as arranging access to your registered account, completing your transactions, and providing you with related benefits,
-special promotions, or customer service.</li>
-
-<li>To respond to your inquiries and fulfill your requests, when you contact us via one of our online contact forms or otherwise, for example, when you send us questions,
-suggestions, compliments or complaints, or when you request information about our Services.</li>
-
-</ul>
-We will engage in these activities to manage our contractual relationship with you and/or to comply with a legal obligation.
-
-Providing you with our marketing materials and facilitating social sharing.
-
-To send you marketing related emails, with information about our services, new products and other news about our company.
-	To facilitate social sharing functionality that you choose to use.
-	We will engage in this activity with your consent or where we have a legitimate interest.
-	Analyzing Personal Information for business reporting and providing personalized services.
-	To analyze or predict our users&apos; preferences in order to prepare aggregated trend reports on how our digital content is used, so we can improve our Services.
-	To better understand your interests and preferences, so that we can personalize our interactions with you and provide you with information and/or offers tailored to your interests.
-
-We may offer you the opportunity to participate in a sweepstakes, contest or other promotion.
-
-	Some of these promotions have additional rules containing information about how we will use and disclose your Personal Information. Please read those additional rules before choosing to participate.
-	We use this information to manage our contractual relationship with you.
-Disclosure of Personal Information
-
-We disclose Personal Information:
-	To our third-party service providers, to facilitate services they provide to us.
-	These can include providers of services such as website hosting, data analysis, fraud prevention, information technology and related infrastructure provision, customer service or related benefits, email delivery, auditing, and other services.
-	To third party sponsors of sweepstakes, contests, and similar promotions.
-	By using the Services, you may elect to disclose Personal Information.
-	While playing the game, on message boards, chat, profile pages, and other services to which you are able to post information and content. Please note that any information you post or disclose through these services will become public and may be available to other users and the general public.
-	Other Uses and Disclosures
-	We also use and disclose your Personal Information as necessary or appropriate, in particular when we have a legal obligation or legitimate interest to do so:
-
-OTHER INFORMATION
-“Other Information” is any information that does not reveal your specific identity or does not directly relate to an identifiable individual. The Services collect Other Information such as:
-	Browser and device information
-	Information collected through cookies, pixel tags and other technologies
-	Demographic information and other information provided by you that does not reveal your specific identity
-Cookies.
-Cookies are pieces of information stored directly on the computer that you are using. Cookies allow us to collect information such as browser type, time spent on the Services, pages visited, language preferences, mouse clicks, page scrolls, keystrokes and other traffic data. We and our service providers use the information for security purposes, to facilitate navigation, to display information more effectively, and to personalize your experience. We also gather statistical information about use of the Services in order to continually improve their design and functionality, understand how they are used, and assist us with resolving questions regarding them. Cookies further allow us to select which of our advertisements or offers are most likely to appeal to you and display them while you are on the Services. We may also use cookies or other technologies in online advertising to track responses to our ads. We do not currently respond to browser do-not-track signals. 
-Analytics. We use third party analytics service providers, including Google Analytics, which use cookies and similar technologies to collect and analyze information about use of the Services and report on activities and trends. These analytics services may also collect information regarding the use of other websites, apps and online resources.
-SECURITY
-We seek to use reasonable organizational, technical and administrative measures to protect Personal Information within our organization. Unfortunately, no data transmission or storage system can be guaranteed to be 100% secure. If you have reason to believe that your interaction with us is no longer secure, please immediately notify us in accordance with the “Contacting Us” section below.
-SENSITIVE INFORMATION
-Unless we request it, we ask that you not send us, and you not disclose, any sensitive Personal Information (e.g., social security numbers, information related to racial or ethnic origin, political opinions, religion or other beliefs, health, biometrics or genetic characteristics, criminal background, or trade union membership) on or through the Services or otherwise to us.
-UPDATES TO THIS PRIVACY POLICY
-The “LAST UPDATED” legend at the top of this Privacy Policy indicates when this Privacy Policy was last revised. Any changes will become effective when we post the revised Privacy Policy on the Services.
-CONTACTING US
-If you have any questions about this Privacy Policy, please contact us at admin@dexbattle.com
-
-        </p>
+            <h1 className="text-blue-400 font-Kanit text-3xl">
+                DB Gaming Privacy Policy
+            </h1>
+            <p className="text-white text-2xl">
+                <span className="font-semibold">LAST UPDATED: November 2022</span>
+                <br/>
+                DB Gaming Limited and its affiliates (“DB,” “we,” or “us”) want you to be familiar with how we collect,
+                use and disclose information. This Privacy Policy describes our practices in connection with information
+                that we collect through:
+                <br/>
+                Websites operated by us from which you are accessing this Privacy Policy (the “Websites”);
+                <br/>
+                HTML-formatted email messages that we send to you that link to this Privacy Policy or other
+                communications with you; and Offline business interactions you have with us.
+                <br/>
+                Collectively, we refer to the Websites, emails, and offline business interactions as the “Services.”
+                <br/>
+                
+            </p>
         </div>
     )
 }
