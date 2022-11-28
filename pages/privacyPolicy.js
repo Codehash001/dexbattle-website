76639cb64bdf38830e308be07474b1be98eb6e45
Codehash001 <codehashblockchain@gmail.com>
2022-11-28 23:14:54 +0530

    update privact policies

--- pages/privacyPolicy.js
+++ pages/privacyPolicy.js
@@ -6,39 +6,64 @@ export default function Privacy() {
             DB Gaming privacy Policy
         </h1>
 
-        <p className="my-4 text-gray-200 font-Kanit text-[24px]">
-            <span className="font-bold my-4 text-gray-200 font-Kanit text-[24px]">LAST UPDATED: November 2022</span>
-DB Gaming Limited and its affiliates (“DB,” “we,” or “us”) want you to be familiar with how we collect, use and disclose information. This Privacy Policy describes our practices in connection with information that we collect through:
+        <p className="my-4 text-gray-200 font-Kanit text-[24px] text-justify">
+            <span className="font-bold my-4 text-gray-200 font-Kanit text-[24px]">LAST UPDATED: November 2022</span><br/>
+DB Gaming Limited and its affiliates (“DB,” “we,” or “us”) want you to be familiar with how we collect, use and disclose information.
+This Privacy Policy describes our practices in connection with information that we collect through:
+<br/>
 Websites operated by us from which you are accessing this Privacy Policy (the “Websites”);
+<br/>
 HTML-formatted email messages that we send to you that link to this Privacy Policy or other communications with you; and Offline business interactions you have with us.
 Collectively, we refer to the Websites, emails, and offline business interactions as the “Services.”
-PERSONAL INFORMATION
+<br/>
+<br/>
+<span className="font-bold">PERSONAL INFORMATION</span>
+<br/>
 “Personal Information” is information that identifies you as an individual or relates to an identifiable individual. The Services collect Personal Information, including:
-Name, Email address, Username and password, Crypto wallet address, including related data used to detect cryptocurrency and NFT holdings, Social media handle, IP address (we may also derive your approximate location from your IP address)
+Name, Email address, Username and password, Crypto wallet address, including related data used to detect cryptocurrency and NFT holdings, Social media handle, IP address
+ (we may also derive your approximate location from your IP address)
+ <br/>
 In-game activity, such as lands visited or acquired, quests and games completed, NFT transactions, and avatars created
+<br/>
+<span className="font-bold">Collection of Personal Information</span>
+<br/>
 
-Collection of Personal Information
+<ul>
+<li>We and our service providers collect Personal Information in a variety of ways, including:</li>
+<li>Through the Services.</li>
+<li>We collect Personal Information through the Services, for example, when you register an account to access the Services, complete a survey, contact customer support,
+or make a purchase on our online marketplace.</li>
+<li>From Other Sources.</li>
+<li>We receive your Personal Information from other sources, for example, crypto wallet providers, when they share the information with us.</li>
+</ul>
 
-	We and our service providers collect Personal Information in a variety of ways, including:
-	Through the Services.
-	We collect Personal Information through the Services, for example, when you register an account to access the Services, complete a survey, contact customer support, or make a purchase on our online marketplace.
-	From Other Sources.
-	We receive your Personal Information from other sources, for example, crypto wallet providers, when they share the information with us.
 Use of Personal Information
-
+<br/>
 We and our service providers use Personal Information for the following purposes:
+<br/>
 Providing the functionality of the Services and fulfilling your requests.
-	To provide the Services&apos; functionality to you, such as arranging access to your registered account, completing your transactions, and providing you with related benefits, special promotions, or customer service.
-	To respond to your inquiries and fulfill your requests, when you contact us via one of our online contact forms or otherwise, for example, when you send us questions, suggestions, compliments or complaints, or when you request information about our Services.
+<ul>
+    
+<li>To provide the Services&apos; functionality to you, such as arranging access to your registered account, completing your transactions, and providing you with related benefits,
+special promotions, or customer service.</li>
+
+<li>To respond to your inquiries and fulfill your requests, when you contact us via one of our online contact forms or otherwise, for example, when you send us questions,
+suggestions, compliments or complaints, or when you request information about our Services.</li>
+
+</ul>
 We will engage in these activities to manage our contractual relationship with you and/or to comply with a legal obligation.
+
 Providing you with our marketing materials and facilitating social sharing.
+
 To send you marketing related emails, with information about our services, new products and other news about our company.
 	To facilitate social sharing functionality that you choose to use.
 	We will engage in this activity with your consent or where we have a legitimate interest.
 	Analyzing Personal Information for business reporting and providing personalized services.
 	To analyze or predict our users&apos; preferences in order to prepare aggregated trend reports on how our digital content is used, so we can improve our Services.
 	To better understand your interests and preferences, so that we can personalize our interactions with you and provide you with information and/or offers tailored to your interests.
+
 We may offer you the opportunity to participate in a sweepstakes, contest or other promotion.
+
 	Some of these promotions have additional rules containing information about how we will use and disclose your Personal Information. Please read those additional rules before choosing to participate.
 	We use this information to manage our contractual relationship with you.
 Disclosure of Personal Information
