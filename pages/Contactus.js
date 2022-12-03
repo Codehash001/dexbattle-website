545d8e90b41bbd86e890d2b10b2230ff664dd87a
Codehash001 <codehashblockchain@gmail.com>
2022-12-03 18:46:01 +0530

    email

--- pages/Contactus.js
+++ pages/Contactus.js
@@ -1,9 +1,90 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/NavAbout";
-
+import {useState} from 'react';
 
 export default function Contact () {
+
+  // States for contact form fields
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  //   Form validation state
+  const [errors, setErrors] = useState({});
+
+  //   Setting button text on form submission
+  const [buttonText, setButtonText] = useState("Send");
+
+  // Setting success or failure messages states
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showFailureMessage, setShowFailureMessage] = useState(false);
+
+  // Validation check method
+  const handleValidation = () => {
+    let tempErrors = {};
+    let isValid = true;
+
+    if (fullname.length <= 0) {
+      tempErrors["fullname"] = true;
+      isValid = false;
+    }
+    if (email.length <= 0) {
+      tempErrors["email"] = true;
+      isValid = false;
+    }
+    if (subject.length <= 0) {
+      tempErrors["subject"] = true;
+      isValid = false;
+    }
+    if (message.length <= 0) {
+      tempErrors["message"] = true;
+      isValid = false;
+    }
+
+    setErrors({ ...tempErrors });
+    console.log("errors", errors);
+    return isValid;
+  };
+
+  //   Handling form submit
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let isValidForm = handleValidation();
+
+    if (isValidForm) {
+      setButtonText("Sending");
+      const res = await fetch("/api/sendgrid", {
+        body: JSON.stringify({
+          email: email,
+          fullname: fullname,
+          subject: subject,
+          message: message,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      });
+
+      const { error } = await res.json();
+      if (error) {
+        console.log(error);
+        setShowSuccessMessage(false);
+        setShowFailureMessage(true);
+        setButtonText("Send");
+        return;
+      }
+      setShowSuccessMessage(true);
+      setShowFailureMessage(false);
+      setButtonText("Send");
+    }
+    console.log(fullname, email, subject, message);
+  };
+  
     return (
 
 <div>
@@ -97,44 +178,102 @@ export default function Contact () {
       </div>
       <div class="w-full px-4 lg:w-1/2 xl:w-5/12">
         <div class="relative rounded-lg bg-white p-8 shadow-lg sm:p-12">
-          <form>
-            <div class="mb-6">
-              <input
-                type="text"
-                placeholder="Your Name"
-                class="text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
-              />
-            </div>
-            <div class="mb-6">
-              <input
-                type="email"
-                placeholder="Your Email"
-                class="text-body-color border-[#f0f0f0] focus:border-gray-700 w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
-              />
-            </div>
-            <div class="mb-6">
-              <input
-                type="text"
-                placeholder="Your Phone"
-                class="text-body-color border-[#f0f0f0] focus:border-gray-700 w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
-              />
-            </div>
-            <div class="mb-6">
-              <textarea
-                rows="6"
-                placeholder="Your Message"
-                class="text-body-color border-[#f0f0f0] focus:border-gray-700 w-full resize-none rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
-              ></textarea>
-            </div>
-            <div>
-              <button
-                type="submit"
-                class="bg-blue-800 border-gray-700 w-full rounded border p-3 text-white transition hover:bg-opacity-90"
+
+        <form
+          onSubmit={handleSubmit}
+          className="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-blue-500"
+        >
+          <h1 className="text-2xl font-bold dark:text-gray-50">
+            Send a message
+          </h1>
+
+          <label
+            htmlFor="fullname"
+            className="text-gray-500 font-light mt-8 dark:text-gray-50"
+          >
+            Full name<span className="text-red-500 dark:text-gray-50">*</span>
+          </label>
+          <input
+            type="text"
+            value={fullname}
+            onChange={(e) => {
+              setFullname(e.target.value);
+            }}
+            name="fullname"
+            className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
+          />
+         
+
+          <label
+            htmlFor="email"
+            className="text-gray-500 font-light mt-4 dark:text-gray-50"
+          >
+            E-mail<span className="text-red-500">*</span>
+          </label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
+          />
+          
+
+          <label
+            htmlFor="subject"
+            className="text-gray-500 font-light mt-4 dark:text-gray-50"
+          >
+            Subject<span className="text-red-500">*</span>
+          </label>
+          <input
+            type="text"
+            name="subject"
+            value={subject}
+            onChange={(e) => {
+              setSubject(e.target.value);
+            }}
+            className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
+          />
+         
+          <label
+            htmlFor="message"
+            className="text-gray-500 font-light mt-4 dark:text-gray-50"
+          >
+            Message<span className="text-red-500">*</span>
+          </label>
+          <textarea
+            name="message"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+            className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
+          ></textarea>
+          
+          <div className="flex flex-row items-center justify-start">
+            <button
+              type="submit"
+              className="px-10 mt-8 py-2 bg-[#130F49] text-gray-50 font-light rounded-md text-lg flex flex-row items-center"
+            >
+              Submit
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="text-cyan-500 ml-2"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                Send Message
-              </button>
-            </div>
-          </form>
+                <path
+                  d="M9.00967 5.12761H11.0097C12.1142 5.12761 13.468 5.89682 14.0335 6.8457L16.5089 11H21.0097C21.562 11 22.0097 11.4477 22.0097 12C22.0097 12.5523 21.562 13 21.0097 13H16.4138L13.9383 17.1543C13.3729 18.1032 12.0191 18.8724 10.9145 18.8724H8.91454L12.4138 13H5.42485L3.99036 15.4529H1.99036L4.00967 12L4.00967 11.967L2.00967 8.54712H4.00967L5.44417 11H12.5089L9.00967 5.12761Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </button>
+          </div>
+        </form>
           <div>
             <span class="absolute -top-10 -right-9 z-[-1]">
               <svg
