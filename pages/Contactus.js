65a5e459b7280fd1a4bccd7abf38dd4975ab9d24
Codehash001 <codehashblockchain@gmail.com>
2022-12-04 13:23:55 +0530

    update website contact details

--- pages/Contactus.js
+++ pages/Contactus.js
@@ -1,89 +1,16 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/NavAbout";
-import {useState} from 'react';
+import { useForm, ValidationError } from "@formspree/react";
 
 export default function Contact () {
 
-  // States for contact form fields
-  const [fullname, setFullname] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
+  const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORM);
 
-  //   Form validation state
-  const [errors, setErrors] = useState({});
 
-  //   Setting button text on form submission
-  const [buttonText, setButtonText] = useState("Send");
-
-  // Setting success or failure messages states
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [showFailureMessage, setShowFailureMessage] = useState(false);
-
-  // Validation check method
-  const handleValidation = () => {
-    let tempErrors = {};
-    let isValid = true;
-
-    if (fullname.length <= 0) {
-      tempErrors["fullname"] = true;
-      isValid = false;
-    }
-    if (email.length <= 0) {
-      tempErrors["email"] = true;
-      isValid = false;
-    }
-    if (subject.length <= 0) {
-      tempErrors["subject"] = true;
-      isValid = false;
-    }
-    if (message.length <= 0) {
-      tempErrors["message"] = true;
-      isValid = false;
-    }
-
-    setErrors({ ...tempErrors });
-    console.log("errors", errors);
-    return isValid;
-  };
-
-  //   Handling form submit
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    let isValidForm = handleValidation();
-
-    if (isValidForm) {
-      setButtonText("Sending");
-      const res = await fetch("/api/mail", {
-        body: JSON.stringify({
-          email: email,
-          fullname: fullname,
-          subject: subject,
-          message: message,
-        }),
-        headers: {
-          "content-type": "application/json",
-        },
-        method: "POST",
-      });
-
-      const { error } = await res.json();
-      if (error) {
-        console.log(error);
-        setShowSuccessMessage(false);
-        setShowFailureMessage(true);
-        setButtonText("Send");
-        return;
-      }
-      setShowSuccessMessage(true);
-      setShowFailureMessage(false);
-      setButtonText("Send");
-    }
-    console.log(fullname, email, subject, message);
-  };
+  if (state.succeeded) {
+    return <p>Thanks for your submission!</p>;
+  }
   
     return (
 
@@ -195,10 +122,6 @@ export default function Contact () {
           </label>
           <input
             type="text"
-            value={fullname}
-            onChange={(e) => {
-              setFullname(e.target.value);
-            }}
             name="fullname"
             className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
           />
@@ -214,11 +137,9 @@ export default function Contact () {
             type="email"
             name="email"
             value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
             className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
           />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
           
 
           <label
@@ -230,10 +151,6 @@ export default function Contact () {
           <input
             type="text"
             name="subject"
-            value={subject}
-            onChange={(e) => {
-              setSubject(e.target.value);
-            }}
             className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
           />
          
@@ -245,18 +162,16 @@ export default function Contact () {
           </label>
           <textarea
             name="message"
-            value={message}
-            onChange={(e) => {
-              setMessage(e.target.value);
-            }}
             className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
           ></textarea>
+          <ValidationError prefix="Message" field="message" errors={state.errors} />
           
           <div className="flex flex-row items-center justify-start">
             <button
               type="submit"
               className="px-10 mt-8 py-2 bg-[#130F49] text-gray-50 font-light rounded-md text-lg flex flex-row items-center"
-            >
+              disabled={state.submitting}
+              >
               Submit
               <svg
                 width="24"
@@ -272,6 +187,7 @@ export default function Contact () {
                 />
               </svg>
             </button>
+            <ValidationError errors={state.errors} />
           </div>
         </form>
           <div>
